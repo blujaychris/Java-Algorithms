f410f2946167ff591104636b7393eb36d329609f
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2021-02-15 11:18:38 +0100

    Implement method for printing out input argument and overload it to take different types of input

--- src/helpers/Printing.java
+++ src/helpers/Printing.java
@@ -1,2 +1,29 @@
-package helpers;public class Printing {
+package helpers;
+
+public class Printing {
+
+    public static void print(boolean parameter) {
+        System.out.println(parameter);
+    }
+
+    public static void print(boolean[] parameter) {
+        System.out.println(parameter);
+    }
+
+    public static void print(String parameter) {
+        System.out.println(parameter);
+    }
+
+    public static void print(String[] parameter) {
+        System.out.println(parameter);
+    }
+
+    public static void print(int parameter) {
+        System.out.println(parameter);
+    }
+
+    public static void print(int[] parameter) {
+        System.out.println(parameter);
+    }
+
 }
