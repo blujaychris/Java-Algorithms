07e1442185a819970578adff39daa21ed7ac7489
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2021-02-15 15:12:38 +0100

    Test isStringPalindrome method in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import static helpers.Printing.print;
+import static popularAlgorithms.Palindrome.isStringPalindrome;
 import static searchingAlgorithms.BinarySearch.binarySearch;
 
 public class Main {
 
     public static void main(String[] args) {
         print(binarySearch(new int[]{1, 5, 3, 8, 2},5));
-
+        print(isStringPalindrome("redivider");
     }
 }
