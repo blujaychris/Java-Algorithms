a5f4c40e35955f9cf5e7dfc0cf7779d0406d36c7
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2021-02-15 15:35:53 +0100

    Add Anagram class in popularAlgorithms package, implement method for checking if String is Anagram and test it in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import static helpers.Printing.print;
+import static popularAlgorithms.Anagram.isStringAnagram;
 import static popularAlgorithms.Fibonacci.returnFibonacciSequence;
 import static popularAlgorithms.Palindrome.isStringPalindrome;
 import static searchingAlgorithms.BinarySearch.binarySearch;
@@ -9,5 +10,6 @@ public class Main {
         print(binarySearch(new int[]{1, 5, 3, 8, 2},5));
         print(isStringPalindrome("redivider"));
         print(returnFibonacciSequence(10));
+        print(isStringAnagram("abcdefg","agbfced"));
     }
 }
--- src/popularAlgorithms/Anagram.java
+++ src/popularAlgorithms/Anagram.java
@@ -1,2 +1,16 @@
-package popularAlgorithms;public class Anagram {
+package popularAlgorithms;
+
+public class Anagram {
+
+    public static boolean isStringAnagram(String firstWord, String secondWord) {
+        StringBuilder storeLetters = new StringBuilder();
+        for (int i = 0; i < firstWord.length(); i++) {
+            for (int j = 0; j < secondWord.length(); j++) {
+                if (firstWord.charAt(i) == secondWord.charAt(j)) {
+                    storeLetters.append(secondWord.charAt(j));
+                }
+            }
+        }
+        return storeLetters.length() == firstWord.length();
+    }
 }
