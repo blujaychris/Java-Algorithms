d286f23c0b638a8691c4f9596d76d1586df37a8f
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2021-02-15 15:13:17 +0100

    Fix typo

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         print(binarySearch(new int[]{1, 5, 3, 8, 2},5));
-        print(isStringPalindrome("redivider");
+        print(isStringPalindrome("redivider"));
     }
 }
